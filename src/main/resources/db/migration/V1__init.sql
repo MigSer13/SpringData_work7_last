abf41e624e643ffd98fd4b663ec0fc06955b8ca2
MigSer13 <avto470@mail.ru>
2021-07-15 19:54:30 +0300

    update init_sql

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,12 +1,24 @@
-create table categories (id bigserial primary key, title varchar(255));
-insert into categories (title)
+create table products (id bigserial primary key, title varchar(255), price int);
+insert into products (title, price)
 values
-('Food');
-
-create table products (id bigserial primary key, title varchar(255), price int, category_id bigint references categories (id));
-insert into products (title, price, category_id)
-values
-('Milk', 85, 1),
-('Bread', 25, 1),
-('Cheese', 450, 1);
+('Bread',39),
+('Apple',13),
+('Pear',17),
+('Grape',270),
+('Cucumber',15),
+('Tomato',15),
+('Loaf',40),
+('Nuts',150),
+('Lemon',150),
+('Juice',100),
+('Sausage', 310),
+('Cookies',95),
+('Potato',60),
+('Carrot',70),
+('Onion',55),
+('Garlic',83),
+('Pasta',72),
+('Rice',72),
+('Milk',69),
+('Cheese', 400);
 
